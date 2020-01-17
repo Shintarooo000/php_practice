6be4e520fc061e97bded10e59dfc2d8aaef59045
Shintarooo000 <kataoka240407@gmail.com>
2020-01-17 04:24:39 +0000

    kadai-2

--- practice.php
+++ practice.php
@@ -33,5 +33,31 @@ echo 'hello php!';
 // 12月を表示する
 echo $calendar_2018["December"];
 
+$name = "片岡";
+if($name == "片岡"){
+    echo "私はあなたの名前です";
+}else{
+    echo "あなたの名前ではありません";
+}
+$total = 0;
+for($i = 0; $i<=10000; $i++){
+    $total += $i;
+}
 
-    ?>
+echo $total;
+
+$fruits = array("apple","strawberry","apricot","persimmon","kiwi fruit");
+foreach($fruits as $fruit){
+ echo $fruit.'\n';
+}
+/* for文の初めの値を定義する */
+$start = 1;
+// for文の終わりの値を定義する
+$end = 100;
+
+for ($i = $start; $i <= $end; $i++) {
+    if ($i % 5 == 0) {
+        echo $i;
+    }
+}
+?>
